6f779320eaa268006299d49a8e8b929969951824
Leonardo <leonardo.segura@cua.uam.mx>
2026-03-12 12:31:54 -0600

    Correción CONTROLLER

--- becas/src/main/java/mx/uam/cua/tysi/integracion/becas/controller/AlumnoController.java
+++ becas/src/main/java/mx/uam/cua/tysi/integracion/becas/controller/AlumnoController.java
@@ -1,45 +1,42 @@
 package mx.uam.cua.tysi.integracion.becas.controller;
 
-
 import mx.uam.cua.tysi.integracion.becas.dto.AlumnoDTO;
 import mx.uam.cua.tysi.integracion.becas.service.AlumnoService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/alumnos")
 public class AlumnoController {
+
     private final AlumnoService alumnoService;
 
     public AlumnoController(AlumnoService alumnoService){
         this.alumnoService = alumnoService;
     }
 
-    @PostMapping
-    public AlumnoDTO createAlumno(@RequestBody AlumnoDTO alumnoDTO){
+    @PostMapping("/alumnos")
+    public AlumnoDTO alumno(@RequestBody AlumnoDTO alumnoDTO){
         return alumnoService.createAlumno(alumnoDTO);
     }
 
-    @GetMapping
+    @GetMapping("/alumnos")
     public List<AlumnoDTO> getAlumnos(){
         return alumnoService.getAlumnos();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/alumnos/{id}")
     public AlumnoDTO getAlumnoById(@PathVariable Long id){
         return alumnoService.getAlumnoById(id);
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping("/alumnos/{id}")
     public AlumnoDTO updateAlumno(@PathVariable Long id, @RequestBody AlumnoDTO alumnoDTO){
         return alumnoService.updateAlumno(id, alumnoDTO);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/alumnos/{id}")
     public void deleteAlumno(@PathVariable Long id){
         alumnoService.deleteAlumno(id);
     }
-
 }
